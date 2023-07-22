35698357e26e9f157c8948782f822e7830ff13a0
13virat <viratgupta1314@gmail.com>
2023-07-22 23:04:38 +0530

    n

--- portfolio/base/urls.py
+++ portfolio/base/urls.py
@@ -5,5 +5,5 @@ urlpatterns=[
     path('', views.home , name="home"),
     path('posts/', views.posts , name="posts"),
     path('post/', views.post , name="post"),
-    path('profile/', views.profile , name="profile")
+    path('profile/', views.profile , name="profile"),
 ]
