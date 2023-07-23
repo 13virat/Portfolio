1bd3902ca373e95def5547cd786fd13070a6c05f
13virat <viratgupta1314@gmail.com>
2023-07-23 17:21:49 +0530

    dd

--- portfolio/base/views.py
+++ portfolio/base/views.py
@@ -3,10 +3,25 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h2>Home</h2>')
+    return render(request,'base/index.html')
 def posts(request):
-    return HttpResponse('<h2>Posts</h2>')
+    posts=[
+        {
+            'headline':'Laboratory Management System',
+            'sub_headline':'Designed built & mantained a the lab managment system for FOI Laboratories'
+        },
+        {
+            'headline':'Online Store - CoursePost Title',
+            'sub_headline':'Online store with paypal payments intergration and guest user shopping'
+        },
+        {
+            'headline':'Membership Website',
+            'sub_headline':'Modulized guide for online courses with step by  step intructions'
+        },
+    ]
+    context={'posts':posts}
+    return render(request,'base/posts.html',context)
 def post(request):
-    return HttpResponse('<h2>Post Title</h2>')
+    return render(request,'base/post.html')
 def profile(request):
-    return HttpResponse('<h2>User Profile</h2>')
+    return render(request,'base/profile.html')
--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -117,6 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL= '/images/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
+MEDIA_ROOT=os.path.join(BASE_DIR,'static/images')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
